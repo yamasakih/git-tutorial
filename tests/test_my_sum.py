84d0b6f5bd0c3f0c70c82b5605ce39af5b6aeb5b
yamasakih <yamasaki.phone@gmail.com>
2023-05-11 11:42:14 +0900

    :art: Add test_my_sum_3 test

--- tests/test_my_sum.py
+++ tests/test_my_sum.py
@@ -10,3 +10,8 @@ def test_my_sum_2():
     actual = my_sum(-1000, 50000)
     expected = 49000
     assert actual == expected
+
+def test_my_sum_3():
+    actual = my_sum(1000, 3000)
+    expected = 4000
+    assert actual == expected
