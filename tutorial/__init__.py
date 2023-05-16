81a560e7d8d093b087514667aa07397259ed2a5f
ynammoku <champagnesquall@gmail.com>
2023-05-16 11:55:45 +0900

    :art: Add docstring-to-my-functions

--- tutorial/__init__.py
+++ tutorial/__init__.py
@@ -2,13 +2,42 @@ __version__ = "0.1.0"
 
 
 def my_sum(a, b):
+    """Add two numbers together.
+
+    Args:
+        a (numeric): The first number.
+        b (numeric): The second number.
+
+    Returns:
+        numeric: The sum of the two numbers.
+    """
     return a + b
 
 
 def my_mean(array):
+    """Calculate average of numbers
+
+    Args:
+        array (list): The array of numbers.
+
+    Raises:
+        ValueError: If the array is empty.
+
+    Returns:
+        float: The average of the numbers.
+    """
     if len(array) == 0:
         raise ValueError("The array must not be empty.")
     return sum(array) / len(array)
 
 def my_minus(a, b):
+    """Subtract one number from another.
+
+    Args:
+        a (numeric): The first number.
+        b (numeric): The second number.
+
+    Returns:
+        numeric: The result of subtracting b from a.
+    """
     return a - b
